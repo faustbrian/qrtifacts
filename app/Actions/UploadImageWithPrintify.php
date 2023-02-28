12f2d53d04fc633f7aaca1edc9ec7c43644f1a2b
Brian Faust <hello@basecode.sh>
2023-02-28 17:30:21 +0200

    chore: wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- app/Actions/UploadImageWithPrintify.php
+++ app/Actions/UploadImageWithPrintify.php
@@ -11,10 +11,13 @@ use Illuminate\Support\Facades\Http;
  */
 final class UploadImageWithPrintify
 {
-    public static function execute(array $data): array
+    public static function execute(string $name, string $contents): array
     {
         return Http::withToken(config('services.printify.token'))
-            ->post('https://api.printify.com/v1/uploads/images.json', $data)
+            ->post('https://api.printify.com/v1/uploads/images.json', [
+                'file_name' => $name,
+                'contents'  => $contents,
+            ])
             ->json();
     }
 }
