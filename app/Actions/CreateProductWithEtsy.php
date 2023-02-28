bfef5217511337f2bfa4b1138033e4e9fc353ca7
Brian Faust <hello@basecode.sh>
2023-02-28 17:14:56 +0200

    chore: wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- app/Actions/CreateProductWithEtsy.php
+++ app/Actions/CreateProductWithEtsy.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace App\Actions;
 
+use Illuminate\Support\Facades\Http;
+
+/**
+ * @see https://developer.etsy.com/documentation/tutorials/listings/
+ * @see https://developers.etsy.com/documentation/reference/#operation/createDraftListing
+ */
 final class CreateProductWithEtsy
 {
     public function execute(array $data): void
     {
-        //
+        Http::withToken(config('services.printify.token'))
+            ->post('https://api.printify.com//v1/shops/{shop_id}/products.json');
     }
 }
--- app/Actions/CreateProductWithPrintify.php
+++ app/Actions/CreateProductWithPrintify.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Actions;
 
+use Illuminate\Support\Facades\Http;
+
+/**
+ * @see https://developers.printify.com/#create-a-new-product
+ */
 final class CreateProductWithPrintify
 {
     public function execute(array $data): void
     {
-        //
+        Http::withToken(config('services.printify.token'))
+            ->post('https://api.printify.com//v1/shops/{shop_id}/products.json');
     }
 }
